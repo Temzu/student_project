333ebb7c0ff2a0c24973b86f883c27b60f74ad35
Temzu <40913z@mail.ru>
2020-10-03 21:04:02 +0300

    update 0.13

--- src/edu/javastudy/studentorder/SaveStudentOrder.java
+++ src/edu/javastudy/studentorder/SaveStudentOrder.java
@@ -5,8 +5,7 @@ import edu.javastudy.studentorder.domain.StudentOrder;
 
 public class SaveStudentOrder {
     public static void main(String[] args) {
-        StudentOrder so = new StudentOrder();
-        
+        buildStudentOrder();
         saveStudentOrder();
     }
 
@@ -21,10 +20,14 @@ public class SaveStudentOrder {
     static void sendFinanceStudentOrder() {
     }
 
-    StudentOrder buildStudentOrder() {
+    static StudentOrder buildStudentOrder() {
         StudentOrder so = new StudentOrder();
         Adult husband = new Adult();
+        husband.setGivenName("Паша");
         so.setHusband(husband);
+
+        String ans = husband.getPersonString();
+        System.out.println(ans);
         return so;
     }
 }
